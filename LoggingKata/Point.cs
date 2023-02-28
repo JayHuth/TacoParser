1f7201ac70ae672101e95213456f6be80a41015f
Justin Huth <Jayhuth96@gmail.com>
2023-02-28 14:07:57 -0600

    Completed

--- LoggingKata/Point.cs
+++ LoggingKata/Point.cs
@@ -1,8 +1,11 @@
-﻿namespace LoggingKata
+﻿using System.Collections.Generic;
+
+namespace LoggingKata
 {
     public struct Point
     {
         public double Longitude { get; set; }
         public double Latitude { get; set; }
+        public IEnumerable<object> latitude { get; set; }
     }
 }
